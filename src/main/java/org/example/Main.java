b62470fd1951958bd43e42b1fccf0e78afbde3e3
jabox510 <jabravo@ucundinamarca.edu.co>
2025-02-25 20:42:37 -0500

    segunda entrega

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,95 +1,136 @@
 package org.example;
 import java.util.Scanner;
 
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         Scanner teclado = new Scanner(System.in);
 
-        int moneda20 = 0, moneda50 = 0, moneda100 = 0, moneda200 = 0, moneda500 = 0;
-        int opcion = 0, moneda = 0;
+        int candidato1 = 0, candidato2 = 0, candidato3 = 0, opcion = 0, voto = 0, influencia = 0;
+        int inter1=0 , tv1=0 , radio1=0  ,inter2 = 0, tv2 = 0, radio2 = 0,inter3 = 0, tv3 = 0, radio3 = 0;
+        int candidato=0;
 
         do {
             System.out.println("\nSeleccione una opción:");
-            System.out.println("1. Ingresar monedas");
-            System.out.println("2. Contar monedas");
-            System.out.println("3. Total por moneda");
-            System.out.println("4. Total ahorrado");
-            System.out.println("5. Terminar");
+            System.out.println("1. VOTAR");
+            System.out.println("2. CONOCER EL COSTO DE  CAMPAÑAS");
+            System.out.println("3. RESULTADOS VOTACIONES");
+            System.out.println("4. TOTAL VOTANTES");
+            System.out.println("5. PORCENTAJES POR CANDIDATO");
+            System.out.println("6. COSTO PROMEDIO CAMPAÑAS");
+            System.out.println("7. VACIAR URNAS");
+            System.out.println("8. TERMINAR OPCION ");
             System.out.print("Opción: ");
             opcion = teclado.nextInt();
-
             switch (opcion) {
+
                 case 1:
                     do {
-                        System.out.println("\nSeleccione la denominación de la moneda:");
-                        System.out.println("1. $20");
-                        System.out.println("2. $50");
-                        System.out.println("3. $100");
-                        System.out.println("4. $200");
-                        System.out.println("5. $500");
-                        System.out.println("6. Terminar");
-                        System.out.print("Moneda: ");
-                        moneda = teclado.nextInt();
-
-                        switch (moneda) {
+                        System.out.println("\nSeleccione una opción:");
+                        System.out.println("1. CANDIDATO 1");
+                        System.out.println("2. CANDIDATO 2");
+                        System.out.println("3. CANDIDATO 3");
+                        System.out.println("4. TERMINAR OPCION ");
+                        System.out.print("Opción: ");
+                        voto = teclado.nextInt();
+                        switch (voto) {
                             case 1:
-                                moneda20++;
-                                break;
+                                candidato1++;
+                                System.out.println("\nSu voto fue influenviado por :");
+                                System.out.println("1.PUBLICIDAD EN INTERNET");
+                                System.out.println("2.PUBLICIDAD EN RADIO");
+                                System.out.println("3.PUBLICIDAD EN TV");
+                                influencia = teclado.nextInt();
+                                switch (influencia) {
+                                    case 1:
+                                        inter1++;
+                                        break;
+                                    case 2:
+                                        tv1++;
+                                        break;
+                                    case 3:
+                                        radio1++;
+                                        break;
+                                }break;
                             case 2:
-                                moneda50++;
-                                break;
+                                candidato2++;
+                                System.out.println("\nSu voto fue influenviado por :");
+                                System.out.println("1.PUBLICIDAD EN INTERNET");
+                                System.out.println("2.PUBLICIDAD EN RADIO");
+                                System.out.println("3.PUBLICIDAD EN TV");
+                                influencia = teclado.nextInt();
+                                switch (influencia) {
+                                    case 1:
+                                        inter2++;
+                                        break;
+                                    case 2:
+                                        tv2++;
+                                        break;
+                                    case 3:
+                                        radio2++;
+                                        break;
+                                }break;
                             case 3:
-                                moneda100++;
-                                break;
+                                candidato3++;
+                                System.out.println("\nSu voto fue influenviado por :");
+                                System.out.println("1.PUBLICIDAD EN INTERNET");
+                                System.out.println("2.PUBLICIDAD EN RADIO");
+                                System.out.println("3.PUBLICIDAD EN TV");
+                                influencia = teclado.nextInt();
+                                switch (influencia) {
+                                    case 1:
+                                        inter3++;
+                                        break;
+                                    case 2:
+                                        tv3++;
+                                        break;
+                                    case 3:
+                                        radio3++;
+                                        break;
+                                }break;
+
                             case 4:
-                                moneda200++;
+                                System.out.println("votacion finalizada");
                                 break;
-                            case 5:
-                                moneda500++;
-                                break;
-                            case 6:
-                                System.out.println("no hay mas monedas");
-                                break;
-                            default:
-                                System.out.println("opcion no valida");
+
+
                         }
-                    } while (moneda != 6);
+
+
+                    }while (voto != 4) ;
+
                     break;
 
                 case 2:
-                    System.out.println("\nconteo de monedas:");
-                    System.out.println("$20 = " + moneda20);
-                    System.out.println("$50 = " + moneda50);
-                    System.out.println("$100 = " + moneda100);
-                    System.out.println("$200 = " + moneda200);
-                    System.out.println("$500 = " + moneda500);
+                    System.out.println("\nseleccione una opcion: :");
+                    System.out.println("1.CANDIDATO 1");
+                    System.out.println("2.CANDIDATO 2");
+                    System.out.println("3.CANDITO 3");
+                    candidato = teclado.nextInt();
+                    switch (candidato) {
+                        case 1:
+                            System.out.println("VALOR TOTAL POR INTERNET"+inter1*700000);
+                            System.out.println("VALOR TOTAL POR TELEVISION"+tv1*600000);
+                            System.out.println("VALOR TOTAL POR INTERNET"+radio1*200000);
+                            break;
+                        case 2:
+                            System.out.println("VALOR TOTAL POR INTERNET"+inter2*700000);
+                            System.out.println("VALOR TOTAL POR TELEVISION"+tv2*600000);
+                            System.out.println("VALOR TOTAL POR INTERNET"+radio2*200000);
+                            break;
+                        case 3:
+                            System.out.println("VALOR TOTAL POR INTERNET"+inter3*700000);
+                            System.out.println("VALOR TOTAL POR TELEVISION"+tv3*600000);
+                            System.out.println("VALOR TOTAL POR INTERNET"+radio3*200000);
+                            break;
+                    }
                     break;
-
-                case 3:
-                    System.out.println("\ntotal por valor:");
-                    System.out.println("$20  = $" + (moneda20 * 20));
-                    System.out.println("$50  = $" + (moneda50 * 50));
-                    System.out.println("$100 = $" + (moneda100 * 100));
-                    System.out.println("$200 = $" + (moneda200 * 200));
-                    System.out.println("$500 = $" + (moneda500 * 500));
-                    break;
-
-                case 4:
-                    int total = (moneda20 * 20) + (moneda50 * 50) + (moneda100 * 100) + (moneda200 * 200) + (moneda500 * 500);
-                    System.out.println("\n Total ahorrado : $" + total);
-                    break;
-
-                case 5:
-                    System.out.println("\n¡ahorro terminado!");
-                    break;
-
-                default:
-                    System.out.println("\nopción no valida");
             }
 
-        } while (opcion != 5);
 
+
+        } while (opcion != 8);
     }
 }
+
